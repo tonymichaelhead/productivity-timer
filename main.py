ee79185f4189b7820ec1df7f2d24893acb40ac9d
tonymichaelhead <tonymichaelhead@gmail.com>
2019-04-27 12:19:35 -0700

    Add logging to text

--- main.py
+++ main.py
@@ -1,23 +1,66 @@
-# import time
-from threading import Timer
-from bullet import Input
+import time
+from datetime import datetime, timedelta
+# from threading import Timer # will probz remove
+from bullet import Bullet, Input
 
-def alert_finished():
-    print(f"Good job!! You did {task} for {time} mins.")
-    log_task(task, time)
+class ProductivityTimer:
+    def __init__(self):
+        self.running = True
 
-def log_task(task, time):
-    f = open("task_log.txt", "w")
-    f.write(f"Did {task} for {time} mins.")
-    f.close()
+    def show_menu(self):
+        cli = Bullet(prompt="What would you like to do?", 
+                     choices=["Do task", "View log", "Exit"], 
+                     bullet="🔥")
+        choice = cli.launch()
 
-cli = Input(prompt="What task? 📝 >", strip=False)
-task = cli.launch()
+        if choice is "Do task":
+            self.do_task()
+        elif choice is "View log":
+            self.read_logs()
+        elif choice is "Exit":
+            self.running = False
+        # print(running)
 
-cli = Input(prompt="How long? ⏰ >", strip=False)
+    def do_task(self):
+        cli = Input(prompt="What task? 📝 >", strip=False)
+        task = cli.launch()
 
-# Set timer
-time = int(cli.launch())
+        cli = Input(prompt="How long? ⏰ >", strip=False)
 
-timer = Timer(time, alert_finished)
-timer.start()
+        # Set timer
+        duration = int(cli.launch())
+
+        try:
+            print("Press CTRL+C to cancel timer and log time.")
+            time.sleep(duration)
+            self.alert_finished(task, duration)
+        except KeyboardInterrupt:
+            print("\nTimer cancelled and time logged!")
+            # TODO: duration passed should time executed before cancelled
+            self.alert_finished(task, duration)
+        
+
+    def alert_finished(self, task, duration):
+        print(f"Good job!! You did {task} for {duration} mins.")
+        self.log_task(task, duration)
+
+    def log_task(self, task, duration):
+        now = datetime.now()
+        time_started = datetime.now() - timedelta(minutes=duration)
+        time_started_formatted = time_started.strftime("%Y-%m-%d %H:%M")
+        f = open("task_log.txt", "a")
+        f.write(f"{time_started_formatted} Did {task} for {duration} mins.\n")
+        f.close()
+
+    def read_logs(self):
+        try:
+            f = open("task_log.txt", "r")
+            print(f.read())
+        except:
+            print("You don't have any logged tasks yet!")
+            
+
+app = ProductivityTimer()
+
+while(app.running):
+    app.show_menu()
